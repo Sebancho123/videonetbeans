e9f917ef92ff8654402d18b3eb1bba5c146bc87c
Sebancho123 <nietomanv@gmail.com>
2024-01-11 16:49:56 -0500

    Ejemplo usando netbeans con git

---
+++ src/main/java/com/sebancho/automovil/Automovil.java
@@ -0,0 +1,16 @@
+
+package com.sebancho.automovil;
+
+import com.sebancho.automovil.igu.Principal;
+
+public class Automovil {
+
+    public static void main(String[] args) {
+        //llamamos y hacemos visible a nuestra igu
+        Principal princ = new Principal();
+        
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+}
